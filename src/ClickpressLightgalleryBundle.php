cb98c1ba82434517e7c29e5e8d60d438a781cb76
stefansl <ssl@clickpress.de>
2019-07-11 18:51:32 +0200

    Initial Commit

---
+++ src/ClickpressLightgalleryBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of LightgalleryBundle.
+ *
+ * (c) Stefan Schulz-Lauterbach
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Clickpress\LightgalleryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ClickpressLightgalleryBundle extends Bundle
+{
+}
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of LightgalleryBundle.
+ *
+ * (c) Stefan Schulz-Lauterbach
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Clickpress\LightgalleryBundle\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(Clickpress\LightgalleryBundle\ClickpressLightgalleryBundle::class)
+                ->setLoadAfter([Contao\CoreBundle\ContaoCoreBundle::class]),
+        ];
+    }
+}
